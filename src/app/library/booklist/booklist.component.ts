8e7f5890ad1f23c84d82137e023d7de5d9d61fb7
CianMc98 <cianmc98@hotmail.com>
2020-12-10 15:24:20 +0000

    booklist components for lib service

--- src/app/library/booklist/booklist.component.ts
+++ src/app/library/booklist/booklist.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BookModel } from 'src/app/shared/book-model';
+import { LibService } from 'src/app/shared/lib.service';
 
 @Component({
   selector: 'app-booklist',
@@ -6,8 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./booklist.component.css']
 })
 export class BooklistComponent implements OnInit {
-
-  constructor() { }
+  author: String;
+  @Input() b: BookModel[];
+  books: Array<BookModel> = [];
+  constructor(private libService:LibService,private activateRoute: ActivatedRoute) { 
+    this.author = this.activateRoute.snapshot.params.id;
+    this.libService.getAllByAuthor(this.author).subscribe(book =>{
+      this.books = book;
+    })
+  }
 
   ngOnInit(): void {
   }
